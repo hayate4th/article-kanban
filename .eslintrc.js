2455435383afac1a1e4b968b7dfd7130b45f93fe
hayate4th <dia.hayayer@gmail.com>
2019-09-05 23:30:55 +0900

    Fix eslintrc to use typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   extends: "airbnb-typescript-prettier",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
   rules: {
     "import/no-extraneous-dependencies": ["error", {
       devDependencies: ["**/*.stories.tsx"],
