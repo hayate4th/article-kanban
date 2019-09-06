d67497eacda8ad1ad6a3b27252194a76a166e5f3
hayate4th <dia.hayayer@gmail.com>
2019-09-06 23:39:38 +0900

    Add styles to design Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,64 @@
 import React from "react";
+import styled from 'styled-components';
 
 interface CardProps {
   title: string;
   url: string;
-  date: string;
+  registerdDate: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, url, date }) => {
-  return <div>
-    <a href={url} target="_blank">{title}</a>
-    <span>{date}</span>
-  </div>;
+const Card: React.FC<CardProps> = ({ title, url, registerdDate }) => {
+  return (
+    <StyledDiv>
+      <StyledA href={url} target="_blank">
+        <StyledH1>{title}</StyledH1>
+        <StyledSpan>登録日: {registerdDate}</StyledSpan>
+      </StyledA>
+    </StyledDiv>);
 };
+
+// TODO: 複数行以上になると text-overflow: ellipsis がかかるようにする
+const StyledH1 = styled.h1`
+  font-size: 25px;
+  margin: 0;
+  max-height: 150px;
+  overflow: hidden;
+  text-align: justify-all;
+  word-break: break-all;
+`;
+
+// TODO: height と width が子要素に依存しまくっているのでしっかりしたい
+const StyledDiv = styled.div`
+  font-size: 25px;
+  height: 204px;
+  width: 204px;
+`;
+
+const StyledA = styled.a`
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  color: black;
+  display: block;
+  height: 180px;
+  padding: 10px;
+  position: relative;
+  text-decoration: none;
+  width: 180px;
+
+  &:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`;
+
+// TODO: span を block 要素にしたくない
+const StyledSpan = styled.span`
+  bottom: 10px;
+  display: block;
+  font-size: 50%;
+  position: absolute;
+  right: 10px;
+`;
+
 
 export default Card;
