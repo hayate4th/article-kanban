ac93bac9b4b43a62f90af6716c70bf56e6795641
hayate4th <dia.hayayer@gmail.com>
2019-09-07 14:19:24 +0900

    Add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./components/Card";
+import App from "./App";
 
-ReactDOM.render(
-  <Card
-    title="Introducing Hooks"
-    url="https://reactjs.org/docs/hooks-intro.html"
-    registerdDate="2019/09/06"
-  />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
